c034978074243f7e4743c4594ab78fd9db47070f
Dyadyusha Artem <artikool5@yandex.ru>
2024-06-16 18:59:26 +0300

    feat(signup-form): добавлен раздел "общая информация"

--- src/components/Signup/SignupForm.tsx
+++ src/components/Signup/SignupForm.tsx
@@ -1,11 +1,31 @@
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import React from 'react';
+import TextInput from '../input/TextInput';
+import FormSubheader from './FormSubheader';
 
 function SignupForm() {
   return (
     <Paper elevation={2} sx={{p: 2, maxWidth: 'fit-content'}}>
       <Stack direction='column' spacing={2}>
+        <FormSubheader>Общая информация</FormSubheader>
+        <Stack direction='row' spacing={2} useFlexGap flexWrap='wrap' >
+          <TextInput
+            label='Фамилия'
+            id='surname-input'
+            required
+            autoFocus
+          />
+          <TextInput
+            label='Имя'
+            id='surname-input'
+            required
+          />
+          <TextInput
+            label='Отчество'
+            id='patronim-input'
+          />
+        </Stack>
       </Stack>
     </Paper>
   );
