9d761227301a945b5ba70ff297c01c4a1d4b5e0e
Dyadyusha Artem <artikool5@yandex.ru>
2024-06-16 15:06:15 +0300

    feat: добавлена кастомная тема

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { blue } from '@mui/material/colors';
+
+declare module '@mui/material/styles' {
+  interface PaletteColor {
+    bg?: string;
+  }
+  interface SimplePaletteColorOptions {
+    bg?: string;
+  }
+};
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+      dark: blue[600],
+      bg: blue[500]+'0A',
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
