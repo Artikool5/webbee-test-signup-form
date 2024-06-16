90d9a54b1614cd6e5150e6c897b0c4bb6cdb59dd
Dyadyusha Artem <artikool5@yandex.ru>
2024-06-16 18:55:10 +0300

    feat(signup-form): добавлен скелет компонента формы

---
+++ src/components/Signup/SignupForm.tsx
@@ -0,0 +1,14 @@
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import React from 'react';
+
+function SignupForm() {
+  return (
+    <Paper elevation={2} sx={{p: 2, maxWidth: 'fit-content'}}>
+      <Stack direction='column' spacing={2}>
+      </Stack>
+    </Paper>
+  );
+}
+
+export default SignupForm;
